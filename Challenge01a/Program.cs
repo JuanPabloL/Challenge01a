2ae304ef93aa8378ec33f81e3cddb9fe85e5b893
JuanPabloL <jp.lopgon@gmail.com>
2024-06-29 15:29:39 +0000

    Enable swagger for testing in app service published.

--- Challenge01a/Program.cs
+++ Challenge01a/Program.cs
@@ -22,11 +22,14 @@ var app = builder.Build();
 EngineContext.Current.Configure(app.Services);
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
